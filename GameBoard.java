446b3e64c2c0fddbabb75de2eb6586f6261a30af
Enigma <33737747+theEngima@users.noreply.github.com>
2017-11-20 15:12:18 -0500

    Add files via upload

--- GameBoard.java
+++ GameBoard.java
@@ -8,6 +8,7 @@ public class GameBoard extends JFrame implements MouseListener, MouseMotionListe
 	
 	/*** Hardcoded Info ***/
 	public static final String NAME = "RISK: The Game of World Domination";
+	public static final String[] TERRITORY_NAMES = {"Alaska", "Alberta", "Central America", "Eastern United States", "Greenland", "Northwest Territory", "Ontario", "Quebec", "Western United States", "Argentina", "Brazil", "Peru", "Venezuela", "Great Britain", "Iceland", "Northern Europe", "Scandinavia", "Southern Europe", "Ukraine", "Western Europe", "Congo", "East Africa", "Egypt", "Madagascar", "North Africa", "South Africa", "Afghanistan", "China", "India", "Irkutsk", "Japan", "Kamchatka", "Middle East", "Mongolia", "Siam", "Siberia", "Ural", "Yakutsk", "Eastern Australia", "Indonesia", "New Guinea", "Western Australia"};
 	public static final boolean[][] ADJACENCY = {};
 	public static final double[] LOCATIONS = {};
 	
@@ -19,8 +20,10 @@ public class GameBoard extends JFrame implements MouseListener, MouseMotionListe
 	/*** Private Member Variables ***/
 	//BoardState
 	private BoardState state;
-	//Image dimensions
+	//Image
 	private int[] imgCorner;
+	private JLabel imgPanel;
+	private ImageIcon img;
 	private int[] imgDim;
 	//Controls
 	private JPanel playerStats;
@@ -34,19 +37,43 @@ public class GameBoard extends JFrame implements MouseListener, MouseMotionListe
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setVisible(true);
+		setLayout(null);
 		
 		//Add controls
 		playerStats = new JPanel(new BorderLayout(BoardState.MAX_PLAYER, 1));
 		controls = new JPanel();
-		this.add(playerStats, BorderLayout.WEST);
-		this.add(controls, BorderLayout.EAST);
+		this.add(playerStats);
+		this.add(controls);
 		playerStats.setBackground(Color.BLACK);
 		controls.setBackground(Color.BLACK);
 		
 		//Set up image
 		imgCorner = new int[2];
 		imgDim = new int[2];
+		imgPanel = new JLabel();
+		img = new ImageIcon("Risk-Map.png");
+		imgDim[1] = getHeight();
+		imgDim[0] = imgDim[1] * img.getIconWidth() / img.getIconWidth();
+		imgCorner[0] = (getWidth() - imgDim[0]) / 2;
+		imgCorner[1] = 0;
+		imgPanel.setBounds(imgCorner[0], imgCorner[1], imgCorner[0] + imgDim[0], imgCorner[1] + imgDim[1]);
+		imgPanel.setIcon(img);
+		add(imgPanel);
 		
+		//Listeners
+		addMouseMotionListener(this);
+		addMouseListener(this);
+		addKeyListener(this);
+		
+	}
+	
+	/*** Draw ***/
+	public void paint(Graphics g){
+		super.paint(g);
+		imgPanel.paint(g);
+		controls.paint(g);
+		playerStats.paint(g);
+		//state.paint(g);
 	}
 	
 	/*** Accessors & Mutators ***/
